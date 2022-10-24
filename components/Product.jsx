f4a3a718bc218c4006b00281377c58cff5aaacbb
Mahmud-CSE16 <mahmudul7959@gmail.com>
2022-10-24 20:05:14 +0600

    init

---
+++ components/Product.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../repository/client";
+
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            className="product-image"
+            src={urlFor(image && image[0])}
+            alt="product image"
+            width={250}
+            height={250}
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">{price}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
